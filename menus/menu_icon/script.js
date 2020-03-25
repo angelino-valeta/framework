06e20d6877b64ee5c14bef6cb94f101d0aa59a9d
Angelino Valeta <angelinovaleta@gmail.com>
2020-03-26 00:21:04 +0100

    fix problem

--- menus/menu_icon/script.js
+++ menus/menu_icon/script.js
@@ -1,7 +1,29 @@
-
+/*
 function hamburguer(x){
 	x.classList.toggle("change");
 	var c = document.querySelector(".content");
 	c.style.opacity = "1";
 }
+*/
+let menuIcon = document.getElementsByClassName('menu-icon')
+
+for(let i = 0; i < menuIcon.length; i++){
+    
+    menuIcon[i].addEventListener('click', function(){
+       // console.log('click')
+        
+        this.classList.toggle('change')
+        
+        let content = this.nextElementSibling
+        
+        if(content.style.display === 'block'){
+            
+            content.style.display = 'none'
+        }else{
+            content.style.display = 'block'
+        }
+
+    })
+}
+
 
