d4375b6e42377e400905615cc35107198d8ac912
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-01 00:30:32 +0100

    show popup when click in click me btn

--- popup/main.js
+++ popup/main.js
@@ -1,3 +1,9 @@
 const btn = document.querySelector('.btn')
 const popup = document.querySelector('.popup-wrapper')
 const close = document.querySelector('.close')
+
+//console.log(popup)
+btn.addEventListener('click', () =>{
+	//console.log('click')
+	popup.style.display = 'block'
+})
